9686e917a725b6a5de61dadcecbf97ca8a3ce26e
Oleksandr Prudnikov <oleksandr.prudnikov@cakealliance.io>
2023-04-14 15:23:11 +0300

    source activation fix try

--- systemd/create_and_install_systemctl_files.py
+++ systemd/create_and_install_systemctl_files.py
@@ -10,18 +10,19 @@ parent_path = dirname(dirname(abspath(__file__)))
 repository_name = os.path.basename(dirname(dirname(abspath(__file__))))
 
 if not os.path.exists(os.path.join(parent_path, 'env')):
+    subprocess.run(['sudo apt-get install python3.10-dev default-libmysqlclient-dev build-essential'], capture_output=True, shell=True)
     print('venv creating started')
     venv_creation_result = subprocess.run(['python3', '-m', 'venv', 'env'], capture_output=True, cwd='..')
     print(venv_creation_result.stdout.decode() + venv_creation_result.stderr.decode())
-    print('=' * 20)
+    print('=' * 70)
     print('venv activation')
     source_activation = subprocess.run(['source env/bin/activate; echo $PATH'], capture_output=True, cwd='..', shell=True)
     print(source_activation.stdout.decode() + source_activation.stderr.decode())
-    print('=' * 20)
+    print('=' * 70)
     print('pip installation')
     pip = subprocess.run(['pip install -r requirements.txt'], capture_output=True, shell=True, cwd='..')
     print(pip.stdout.decode() + pip.stderr.decode())
-    print('=' * 20)
+    print('=' * 70)
 
 
 if not os.path.exists(os.path.join(parent_path, '.env')):
